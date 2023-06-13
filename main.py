0701e5fe856e0edb4eaf8d4d0f017d765f348e4a
Dragos313 <pana749@gmail.com>
2023-06-13 13:47:38 +0300

    Proiect final

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import numpy as np
 import sys
 import os
 from PyQt5.QtWidgets import *
+from PyQt5 import QtGui
 from PyQt5 import uic
 
-A = np.array([[1,1,3],[0,1,3],[-1,3,0]])
-b = np.array([[1],[3],[5]])
 
 class DespreWindow(QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("Despre.ui", self)
+        self.setWindowIcon(QtGui.QIcon('matrix.png'))
         self.setWindowTitle("Despre")
 
 class MyGUI(QMainWindow):
@@ -18,7 +18,7 @@ class MyGUI(QMainWindow):
     def __init__(self):
         super(MyGUI, self).__init__()
         uic.loadUi("proiectGUI.ui", self)
-        
+        self.setWindowIcon(QtGui.QIcon('matrix.png'))
         self.setWindowTitle("Calculator matrice")
         self.UiComponents()
         
@@ -89,26 +89,33 @@ class MyGUI(QMainWindow):
         if(self.rbtManual.isChecked()):
             self.txtManualA.setEnabled(True)
             self.txtManualB.setEnabled(True)
+            self.btnIntroducere.setEnabled(True)
         else:
             self.txtManualA.setEnabled(False)
             self.txtManualB.setEnabled(False)
         if(self.rbtFisier.isChecked()):
             self.btnAlegeFisier.setEnabled(True)
+            self.btnIntroducere.setEnabled(True)
         else:
             self.btnAlegeFisier.setEnabled(False)
+        if(self.rbtAutomat.isChecked()):
+            self.btnCalculeaza.setEnabled(True)
+            self.btnIntroducere.setEnabled(False)
     
     def introducereMatrice(self):
         linii = self.txtLinii.text()
         coloane = self.txtColoane.text()
+        
         if(linii == "" and  coloane == "" or linii != coloane):
             message = QMessageBox()
             message.setText("Numarul de linii si/sau coloane este invalid.")      
             message.exec_()
             return
+        
         l = int(float(linii))
         c = int(float(coloane))
-        A = []
-        b = []
+        
+        
         if(self.rbtManual.isChecked()):
             matriceA = self.txtManualA.text()
             matriceB = self.txtManualB.text()
@@ -116,9 +123,11 @@ class MyGUI(QMainWindow):
             A = np.array(elemeteleMatA).reshape(l, c)
             elemeteleMatB = list(map(int, matriceB.split(" ")))
             b = np.array(elemeteleMatB).reshape(l, c - l + 1)
+            
         elif(self.rbtAutomat.isChecked()):
             A = np.random.randint(1, 20,(l, c))
             b = np.random.randint(1, 20,(l, c - l + 1))
+            
         elif(self.rbtFisier.isChecked()):
             caleFisier = self.txtFisier.text()
             with open(caleFisier) as cf:
@@ -128,6 +137,7 @@ class MyGUI(QMainWindow):
                 elemeteleMatB = list(map(int, fisier[1].split(" ")))
                 b = np.array(elemeteleMatB).reshape(l, c - l + 1)
             cf.close()
+            
         self.txtMatriceExtinsa.setText(str(A))
         self.txtMatriceExtinsa_2.setText(str(b))
         self.btnCalculeaza.setEnabled(True)
@@ -136,8 +146,10 @@ class MyGUI(QMainWindow):
     def calculeazaMatrice(self):
         A, b = self.introducereMatrice() 
         rezultatArr = []
+        
         qm = QMessageBox()
         ret = qm.question(self,'', "Vrei sa folosesc pivotarea?", qm.Yes | qm.No)
+        
         if A.shape[0] != A.shape[1]:
             return
         if b.shape[1] > 1 or b.shape[0] != A.shape[0]:
@@ -181,81 +193,3 @@ def main():
     app.exec_()
 
 main()
-
-"""
-def eliminare_gauss(A, b):
-    if A.shape[0] != A.shape[1]:
-        return
-    if b.shape[1] > 1 or b.shape[0] != A.shape[0]:
-        return
-    
-    n = len(b)
-    m = n-1
-    i = 0
-    j = i-1
-    x = np.zeros(n)
-    
-    matrice_extinsa = np.concatenate((A, b), axis = 1, dtype= float)
-    print(matrice_extinsa)
-    
-    while i<n:
-        if matrice_extinsa[i][i] == 0.0:
-            return
-        for j in range(i+1, n):
-            coeficient_scalare = matrice_extinsa[j][i] / matrice_extinsa[i][i]
-            matrice_extinsa[j] = matrice_extinsa[j] - (coeficient_scalare * matrice_extinsa[i])
-            print(matrice_extinsa)
-        i = i + 1
-        
-    x[m] = matrice_extinsa[m][n] / matrice_extinsa[m][m]
-    
-    for k in range (n-2, -1, -1):
-        x[k] = matrice_extinsa[k][n]
-        for j in range(k + 1, n):
-            x[k] = x[k] - matrice_extinsa[k][j] * x[j]
-        x[k] = x[k] / matrice_extinsa[k][k]
-    
-    for rezultat in range(n):
-        print(f"x{rezultat} is {x[rezultat]}")   
-                     
-    
-eliminare_gauss(A, b)
-
-def eliminare_gaussPP(A, b):
-    if A.shape[0] != A.shape[1]:
-        return
-    if b.shape[1] > 1 or b.shape[0] != A.shape[0]:
-        return
-    n = len(b)
-    m = n-1
-    i = 0
-    x = np.zeros(n)
-    
-    matrice_extinsa = np.concatenate((A, b), axis = 1, dtype= float)
-    print(matrice_extinsa)
-    
-    while i<n:
-        for p in range(i+1, n):
-            if abs(matrice_extinsa[i,i]) < abs(matrice_extinsa[p,i]):
-                matrice_extinsa[[p, i]] = matrice_extinsa[[i, p]]
-        
-        if matrice_extinsa[i][i] == 0.0:
-            return
-        for j in range(i+1, n):
-            coeficient_scalare = matrice_extinsa[j][i] / matrice_extinsa[i][i]
-            matrice_extinsa[j] = matrice_extinsa[j] - (coeficient_scalare * matrice_extinsa[i])
-            print(matrice_extinsa)
-        i = i + 1
-        
-    x[m] = matrice_extinsa[m][n] / matrice_extinsa[m][m]
-    
-    for k in range (n-2, -1, -1):
-        x[k] = matrice_extinsa[k][n]
-        for j in range(k + 1, n):
-            x[k] = x[k] - matrice_extinsa[k][j] * x[j]
-        x[k] = x[k] / matrice_extinsa[k][k]
-    
-    for rezultat in range(n):
-        print(f"x{rezultat} is {x[rezultat]}")      
-eliminare_gaussPP(A, b)        
-"""
